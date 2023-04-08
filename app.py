58665cb27cb0149ac29eda371fe7538937222364
Norman Ncube <nmncube@gmail.com>
2023-04-08 10:13:25 +0200

    app.py changesand Dockerfile

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+# Import flask module
 from flask import Flask
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    return '<h1>Hello World from Python!<h1>'
+ 
+# main driver function
+if __name__ == "__main__":
+    app.run()
